7c0c62f71ee1eab9af08765ec87504d902b33ffa
root <root@PremsLaptop.localdomain>
2023-05-07 20:07:13 -0400

    package and server.js created

---
+++ server.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node 
+
+import minimist from 'minimist';
+import express from 'express';
+import {rps, rps;s} from './lib/rpsls.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const agrs = minimist(process.argv.slice(2));
+
+const port = args.port || 5000;
+
+app.get('/app/', (req, res) => {
+	res.status(200);
+	res.send('200 OK');
+});
+
+
